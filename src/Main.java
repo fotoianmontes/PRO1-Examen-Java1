80f73696413c3d861461480ef5c03a7c5749eb2d
Ian Montes <fotoianmontes@gmail.com>
2023-02-22 15:08:43 +0000

    Initial commit, numericOperations.java created

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Realiza un programa en JAVA que cree un array de 10 números leídos por teclado y mostrar la suma
+        de los números pares, la multiplicación de los números impares y ordenar el array de forma ascendente.
+         */
+
+        int[] myArray = new int[10];
+
+
+    }
+}
